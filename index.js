86ccb960ce8f30253bed928a9d8e966535080c44
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2023-11-02 17:25:39 +0600

    Send pagination parameters to the server using query

--- index.js
+++ index.js
@@ -31,8 +31,13 @@ async function run() {
     const productCollection = client.db('emaJohnDB').collection('products');
 
     app.get('/products', async(req, res) => {
+        console.log(req.query);
         const result = await productCollection.find().toArray();
         res.send(result);
+    })
+    app.get('/productsCount', async(req,res)=>{
+      const count = await productCollection.estimatedDocumentCount()
+      res.send({count})
     })
 
     // Send a ping to confirm a successful connection
